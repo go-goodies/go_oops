4b52884a76c3fa01388b44e9498fd14c0d00df26
l3x-venom <lex.sites@gmail.com>
2014-08-28 18:33:28 -0400

    add factory_method and factory_method_test

---
+++ factory_method.go
@@ -0,0 +1,52 @@
+package go_oops
+
+// maker_iface defines makeWidget and registerWidget methods
+type maker_iface interface {
+	makeWidget(w widget) widget_iface
+	registerWidget(widget_iface)
+}
+
+// widget_iface defines the getInfo method
+type widget_iface interface {
+	getInfo() widget
+}
+
+// factory has the create method, used to create and store widgets
+type factory struct {
+}
+
+// factory method lets a class defer instantiation (dependency injection)
+// The concrete function used to create widgets is encapsulated
+func (self *factory) create(factory maker_iface, w widget) widget_iface {
+	widget := factory.makeWidget(w)
+	factory.registerWidget(widget)
+	return widget
+}
+
+// widget is the domain type that this example maniuplates
+type widget struct {
+	name string
+	count int
+}
+// getInfo is the the widget's only method
+func (w *widget) getInfo() widget {
+	return *w
+}
+
+// widgetFactory used to create and store widgets
+type widgetFactory struct {
+	*factory
+	widgets []*widget
+}
+
+// makeWidget takes a pointer to the widgetFactory and returns the new widget
+func (self *widgetFactory) makeWidget(w widget) widget_iface {
+	return &widget{w.name, w.count}
+}
+
+// registerWidget takes a pointer to the widgetFactory and stores the new widget in the factory
+// Note: this state may not be suitable for most functional programming paradigms
+func (wf *widgetFactory) registerWidget(wi widget_iface) {
+	widget := wi.getInfo()
+	wf.widgets = append(wf.widgets, &widget)
+}
---
+++ factory_method_test.go
@@ -0,0 +1,35 @@
+package go_oops
+
+import (
+	"testing"
+	"github.com/remogatto/prettytest"
+)
+
+type mySuite struct {
+	prettytest.Suite
+}
+
+func TestRunner(t *testing.T) {
+	prettytest.Run(t, new(mySuite))
+}
+
+func (s *mySuite) TestFactoryMethod() {
+
+	expected_widgets := []widget{
+		widget{"A", 100},
+		widget{"B", 250},
+		widget{"C", 300}}
+
+	factory := &widgetFactory{}
+	widgets := []widget_iface{
+		factory.create(factory, widget{"A", 100}),
+		factory.create(factory, widget{"B", 250}),
+		factory.create(factory, widget{"C", 300}),
+	}
+
+	for i, widget := range widgets {
+		s.Equal(widget.getInfo(), expected_widgets[i])
+	}
+}
+
+
